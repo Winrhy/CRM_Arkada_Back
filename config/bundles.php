9b5ff50839e447499bc611a7140722eacb31ccbc
Yoann Gimenez <ygimenez10@gmail.com>
2023-11-21 23:07:51 +0100

    authentication WIP (register, login, logout, jwt = OK / sécurité avec role = bug commenté)

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,7 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
