90308d49fdc074cab5ada6299d941e2b15e60fc8
Winrhy <Winrhy@gmail.com>
2023-11-21 10:28:38 +0100

    :sparkles: Add new method, Contact_CRUD Post

--- src/Controller/Api/ContactController.php
+++ src/Controller/Api/ContactController.php
@@ -3,30 +3,72 @@
 namespace App\Controller\Api;
 
 use App\Entity\Contact;
+use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Uid\UuidV6 as Uuid;
 
 
 #[Route('/contact')]
 class ContactController extends AbstractController
 {
-    /*// Create a new contact
-    // POST /contact
-    // This method handles the creation of a new contact. It expects the contact data to be sent in the request body.
-    #[Route('/', name: 'contact_create', methods: ['POST'])]
-    public function create(Request $request): JsonResponse
-    {
-        // Handle the request and create a new Contact entity
+    /**
+     * Create a new contact.
+     *
+     * @param Request                $request
+     * @param EntityManagerInterface $entityManager
+     * @param SerializerInterface    $serializer
+     * @param ValidatorInterface     $validator
+     *
+     * @return JsonResponse
+     *
+     * @Route('', name: 'contact_create', methods: ['POST'])
+     */
+    #[Route('', name: 'contact_create', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse {
+        // Get JSON data from the request
+        $contactData = $request->getContent();
+
+        try {
+            // Deserialize the data into a Contact object
+            $contact = $serializer->deserialize($contactData, Contact::class, 'json');
+        } catch (\Exception $e) {
+            // Handle deserialization errors
+            return $this->json(['error' => 'Invalid JSON: ' . $e->getMessage()], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        // Validate the Contact object
+        $errors = $validator->validate($contact);
+        if (count($errors) > 0) {
+            // If there are validation errors, return them
+            $errorMessages = [];
+            foreach ($errors as $error) {
+                $errorMessages[$error->getPropertyPath()] = $error->getMessage();
+            }
+            return $this->json(['errors' => $errorMessages], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
+        // Persist the Contact object in the database
+        $createdAt = new \DateTimeImmutable();
+        $contact->setCreatedAt($createdAt);
+        $entityManager->persist($contact);
+        $entityManager->flush();
+
+        // Return a success response
+        return $this->json([
+            'message' => 'Contact created successfully',
+            'contact' => $contact
+        ], JsonResponse::HTTP_CREATED);
     }
 
-    // Read a specific contact by ID
+
+    /*// Read a specific contact by ID
     // GET /contact/{id}
     // This method retrieves the details of a specific contact by its ID.
     #[Route('/{id}', name: 'contact_read', methods: ['GET'])]
@@ -52,44 +94,6 @@ class ContactController extends AbstractController
     {
         // Delete the contact
     }*/
-
-    #[Route('', name: 'contact_create', methods: ['POST'])]
-    /**
-     * Create a new contact
-     * POST /contact
-     * This method handles the creation of a new contact. It expects the contact data to be sent in the request body.
-     *
-     * @param Request $request The HTTP request
-     * @param EntityManagerInterface $entityManager
-     * @param SerializerInterface $serializer
-     * @param ValidatorInterface $validator
-     * @return JsonResponse
-     *
-     * @Route("/", name="contact_create", methods={"POST"})
-     */
-    public function create(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer, ValidatorInterface $validator): JsonResponse    {
-        $contactData = $request->getContent();
-
-        // Deserialize the request content into a Contact entity
-        $contact = $serializer->deserialize($contactData, Contact::class, 'json');
-
-        // Validate the entity
-        $errors = $validator->validate($contact);
-        if (count($errors) > 0) {
-            // Handle validation errors
-            return $this->json(['errors' => (string) $errors], JsonResponse::HTTP_BAD_REQUEST);
-        }
-
-        // Persist the new contact
-        $entityManager->persist($contact);
-        $entityManager->flush();
-
-        // Return a success response
-        return $this->json([
-            'message' => 'Contact created successfully',
-            'contact' => $contact
-        ], JsonResponse::HTTP_CREATED);
-    }
 
     // List all contacts
     // GET /contact/list
