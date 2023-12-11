eba261aa6bcd0315ed740383e277db712a0bd5a0
Winrhy <115533484+Winrhy@users.noreply.github.com>
2023-12-11 10:23:57 +0100

    Revert "Crud template mail"

--- src/Entity/MailTemplate.php
+++ src/Entity/MailTemplate.php
@@ -7,8 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Serializer\Annotation\Groups;
-use Symfony\Component\Uid\UuidV7 as Uuid;
+use Symfony\Component\Uid\UuidV6 as Uuid;
 
 #[ORM\Entity(repositoryClass: MailTemplateRepository::class)]
 class MailTemplate
@@ -37,9 +36,6 @@ class MailTemplate
 
     #[ORM\OneToMany(mappedBy: 'template_id', targetEntity: AttachmentTemplateMailAssociation::class)]
     private Collection $attachmentTemplateMailAssociations;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $template_name = null;
 
     public function __construct()
     {
@@ -156,18 +152,6 @@ class MailTemplate
                 $attachmentTemplateMailAssociation->setTemplateId(null);
             }
         }
-
-        return $this;
-    }
-
-    public function getTemplateName(): ?string
-    {
-        return $this->template_name;
-    }
-
-    public function setTemplateName(?string $template_name): static
-    {
-        $this->template_name = $template_name;
 
         return $this;
     }
