f4c9f4ccde8a85869c998fb9d12ebc7baea299b9
Winrhy <Winrhy@gmail.com>
2023-12-12 17:22:56 +0100

    :recycle: Add Service folder and create CompanyServices

--- src/Service/CompanyService.php
+++ src/Service/CompanyService.php
@@ -1,45 +1,49 @@
 <?php
-// src/DTO/CompanyDTO.php
 
-namespace App\DTO;
+namespace App\Service;
 
-class CompanyDTO
+use App\Entity\Company;
+use App\Repository\CompanyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class CompanyService
 {
-    public $name;
-    public $address;
-    public $country;
-    public $city;
-    public $postalCode;
-    public $sector;
-    public $size;
-    public $website;
-    public $phoneNumber;
-    public $email;
-    public $type;
+    private $companyRepository;
+    private $entityManager;
 
-    public function __construct(
-        string $name,
-        string $address,
-        string $country,
-        string $city,
-        string $postalCode,
-        string $sector,
-        int $size,
-        ?string $website,
-        ?string $phoneNumber,
-        ?string $email,
-        ?string $type
-    ) {
-        $this->name = $name;
-        $this->address = $address;
-        $this->country = $country;
-        $this->city = $city;
-        $this->postalCode = $postalCode;
-        $this->sector = $sector;
-        $this->size = $size;
-        $this->website = $website;
-        $this->phoneNumber = $phoneNumber;
-        $this->email = $email;
-        $this->type = $type;
+    public function __construct(CompanyRepository $companyRepository, EntityManagerInterface $entityManager)
+    {
+        $this->companyRepository = $companyRepository;
+        $this->entityManager = $entityManager;
     }
+
+    public function createCompany(Company $company): void
+    {
+        // Vous pouvez ajouter ici de la logique supplémentaire avant la persistance
+        $this->entityManager->persist($company);
+        $this->entityManager->flush();
+    }
+
+    public function updateCompany(Company $company): void
+    {
+        // La mise à jour est généralement gérée automatiquement, mais vous pouvez ajouter de la logique ici si nécessaire
+        $this->entityManager->flush();
+    }
+
+    public function deleteCompany(Company $company): void
+    {
+        $this->entityManager->remove($company);
+        $this->entityManager->flush();
+    }
+
+    public function getCompany(int $id): ?Company
+    {
+        return $this->companyRepository->find($id);
+    }
+
+    public function getAllCompanies(): array
+    {
+        return $this->companyRepository->findAll();
+    }
+
 }
