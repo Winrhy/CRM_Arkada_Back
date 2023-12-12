113db932b67fd8339f04fbab0deaf29bbdc975e8
Paul_Delamare <paulodela2006@gmail.com>
2023-12-13 00:26:16 +0100

    Return all design template to the front

--- src/Controller/Api/TemplateEmailController.php
+++ src/Controller/Api/TemplateEmailController.php
@@ -12,6 +12,9 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
 
 #[Route('/template', name: 'app_template_email')]
 class TemplateEmailController extends AbstractController
@@ -44,30 +47,54 @@ class TemplateEmailController extends AbstractController
     }
 
     #[Route('/create', name: 'app_template_create', methods: ['POST'])]
-    public function createTemplate(EntityManagerInterface $em, Request $request, UserRepository $userRepository): JsonResponse
+    public function createTemplate(EntityManagerInterface $em, Request $request, UserRepository $userRepository, JWTTokenManagerInterface $jwtManager, TokenStorageInterface $tokenStorage): JsonResponse
     {
-        $user = $userRepository->findOneBy(['id'=>"018c5a9f-15ea-7721-8139-0f8bc952c2a5"]);
-        $data = json_decode($request->getContent(), true);
-        $subject = $data['subject'] ?? 'Arkada Studio';
-        $body = $data['body'] ?? '';
-        $from = $data['from'] ?? 'arkada@gmail.com';
-        $template = $data['template'] ?? 'signup.html.twig';
+        try {
+            $token = $request->headers->get('Authorization');
+            $jwtToken = str_replace('Bearer ', '', $token);
+            $user = $userRepository->findOneBy(['jwt_token' => $jwtToken]);
+            $data = json_decode($request->getContent(), true);
+            $name = $data['name'];
+            $subject = $data['subject'] ;
+            $body = $data['body'];
+            $from = $data['from'] ?? 'arkada@gmail.com';
+            $template = $data['design'];
+            $template .= '.html.twig';
+            $projectDir = $this->getParameter('kernel.project_dir');
 
-        $email = new MailTemplate();
-        $email->setSubject($subject);
-        $email->setBody($body);
-        $email->setUserId($user);
-        $email->setSenderMail($from);
-        $email->setTemplateName($template);
-        $email->setName('Exemple 2');
-        $em->persist($email);
-        $em->flush();
+            if (!$user) {
+                throw new \Exception('Utilisateur introuvable.');
+            }
 
-        $response = [
-            'status' => 'success',
-            'message' => 'Modèle d\'e-mail créé avec succès'
-        ];
-        return $this->json($response);
+            $templatePath = $projectDir . '/templates/email/' . $template;
+
+            if (!file_exists($templatePath)) {
+                throw new \Exception("Le template $template n'existe pas.");
+            }
+
+            $email = new MailTemplate();
+            $email->setSubject($subject);
+            $email->setBody($body);
+            $email->setUserId($user);
+            $email->setSenderMail($from);
+            $email->setTemplateName($template);
+            $email->setName($name);
+            $em->persist($email);
+            $em->flush();
+
+            $response = [
+                'status' => 'success',
+                'message' => 'Modèle d\'e-mail créé avec succès'
+            ];
+            return $this->json($response);
+
+        } catch (\Exception $e) {
+            $errorResponse = [
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ];
+            return $this->json($errorResponse, JsonResponse::HTTP_BAD_REQUEST);
+        }
     }
 
     #[Route('/update/{id}', name: 'app_template_update', methods: ['PUT'])]
@@ -101,11 +128,9 @@ class TemplateEmailController extends AbstractController
     #[Route('/delete/{id}', name: 'app_template_delete', methods: ['DELETE', 'GET'])]
     public function deleteTemplate(EntityManagerInterface $em, MailTemplateRepository $templateRepository, UserRepository $userRepository, string $id): JsonResponse
     {
-        $user = $userRepository->findOneBy(['id' => "018c5a9f-15ea-7721-8139-0f8bc952c2a5"]);
-
-        if (!$this->getUser() || !$this->getUser()->isGranted('ROLE_ADMIN')) {
-            return $this->json(['status' => 'error', 'message' => 'Vous n\'avez pas les droits pour supprimer ce modèle'], 403);
-        }
+//        if (!$this->getUser() || !$this->getUser()->isGranted('ROLE_ADMIN')) {
+//            return $this->json(['status' => 'error', 'message' => 'Vous n\'avez pas les droits pour supprimer ce modèle'], 403);
+//        }
 
         $template = $templateRepository->findOneBy(['id'=>$id]);
         if (!$template) {
@@ -116,6 +141,25 @@ class TemplateEmailController extends AbstractController
         $em->flush();
 
         return $this->json(['status' => 'success', 'message' => 'Modèle d\'e-mail supprimé avec succès'], 200);
+    }
+
+    #[Route('/design', name: 'app_templates_design', methods: ['GET'])]
+    public function getTemplateDesign(MailTemplateRepository $templateRepository):JsonResponse
+    {
+        $cheminTemplatesEmail = $this->getParameter('kernel.project_dir') . '/templates/email';
+        $finder = new Finder();
+        $fichiersEmail = $finder->files()
+            ->in($cheminTemplatesEmail)
+            ->name('*.html.twig')
+            ->notPath('/layout/');
+
+        $designs = [];
+
+        foreach ($fichiersEmail as $fichier) {
+            $nameWithoutExtension = str_replace('.html.twig', '', $fichier->getFilename());
+            $designs[] = $nameWithoutExtension;
+        }
+        return $this->json($designs);
     }
 
 }
