c1e5b1d63c957fe14a88ce522f33d1a31638c7f2
Winrhy <Winrhy@gmail.com>
2023-12-13 11:40:07 +0100

    :recycle: refactor Mailling Create a bulk to send mail

--- src/Controller/Api/MailingController.php
+++ src/Controller/Api/MailingController.php
@@ -2,171 +2,71 @@
 
 namespace App\Controller\Api;
 
-use App\Entity\Mail;
-use App\Repository\MailRepository;
-use App\Repository\MailTemplateRepository;
-use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Component\Finder\Finder;
+use App\Service\MaillingService;
+use App\DTO\MaillingDTO;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/mail', name: 'app_mail')]
 class MailingController extends AbstractController
 {
+    private $MaillingService;
+
     /**
-     * Sends an email using a specified template.
+     * Constructor.
      *
-     * @param MailerInterface $mailer The Mailer interface to send emails.
-     * @param string $emailId Unique identifier of the email.
-     * @param string $from Sender's email address.
-     * @param string $to Recipient's email address.
-     * @param string $subject Subject of the email.
-     * @param string $template Name of the email template to be used.
-     * @param string $name Name of the user the email is intended for.
-     * @param string $body Body of the email (optional).
-     *
-     * @return JsonResponse Returns a JSON response indicating success or failure of the email sending process.
+     * @param MaillingService $MaillingService Service for sending emails.
      */
-    private function sendEmail(
-        MailerInterface $mailer,
-        string $emailId,
-        string $from,
-        string $to,
-        string $subject,
-        string $template,
-        string $name,
-        string $body = ''
-    ): JsonResponse {
-        $templateDir = $this->getParameter('kernel.project_dir') . '/templates/email';
-
-        $email = (new TemplatedEmail())
-            ->from($from)
-            ->to(new Address($to))
-            ->subject($subject)
-            ->htmlTemplate('email/' . $template)
-            ->context([
-                'expiration_date' => new \DateTime('+7 days'),
-                'username' => $name,
-                'body' => $body,
-                'email_id' => $emailId,
-            ]);
-
-        try {
-            $mailer->send($email);
-
-            return $this->json([
-                'message' => 'Email envoyé avec succès !',
-            ]);
-        } catch (\Exception $e) {
-            return $this->json([
-                'error' => 'Une erreur est survenue lors de l\'envoi de l\'email.',
-                'message' => $e->getMessage(),
-                'emailId' => $emailId,
-            ], 500);
-        }
+    public function __construct(MaillingService $MaillingService)
+    {
+        $this->MaillingService = $MaillingService;
     }
 
     /**
      * Creates and sends an email from a user-defined payload.
      *
-     * @param EntityManagerInterface $em Entity Manager for database interactions.
-     * @param Request $request The HTTP request object containing email data.
-     * @param MailerInterface $mailer Mailer service for sending emails.
-     * @param UserRepository $userRepository Repository for user entity.
-     * @param MailRepository $mailRepository Repository for mail entity.
+     * This method receives an HTTP POST request with JSON data containing email parameters.
+     * It creates an email based on the provided data and sends it using the MaillingService.
+     *
+     * @param Request $request The HTTP request object containing email data in JSON format.
      *
      * @return JsonResponse Returns a JSON response after creating and sending the email.
      */
     #[Route('/create', name: 'app_mail_create')]
-    public function createEmail(
-        EntityManagerInterface $em,
-        Request $request,
-        MailerInterface $mailer,
-        UserRepository $userRepository,
-        MailRepository $mailRepository
-    ): JsonResponse {
-        $user = $userRepository->findOneBy(['id' => '018c5863-bccc-7b2b-93a7-94f4ff365f87']);
-        $data = json_decode($request->getContent(), true);
-        $from = $data['from'];
-        $to = $data['to'];
-        $subject = $data['subject'] ?? 'Arkada Studio';
-        $template = $data['template'] ?? 'signup.html.twig';
-        $name = $data['name'];
-        $body = $data['body'] ?? '';
+    public function createEmail(Request $request, MaillingService $maillingService): JsonResponse
+    {
+        $emailData = json_decode($request->getContent(), true);
 
-        $email = new Mail();
-        $email->setSubject($subject)
-            ->setRead(false)
-            ->setBody($body)
-            ->setReceiver($to)
-            ->setUserId($user)
-            ->setTimestamp(new \DateTimeImmutable())
-            ->setSenderMail('arkada@gmail.com')
-            ->setDeadPixelId(1);
+        if ($emailData !== null) {
 
-        $em->persist($email);
-        $em->flush();
-        $emailId = $email->getId();
+            $maillingDTOs = [];
 
-        $response = $this->sendEmail(
-            $mailer,
-            $emailId,
-            $from,
-            $to,
-            $subject,
-            $template,
-            $name,
-            $body
-        );
+            $toRecipients = $emailData['to'];
+            foreach ($toRecipients as $recipient) {
+                print('test');
+                $maillingDTO = new MaillingDTO();
+                $maillingDTO->from = $emailData['from'];
+                $maillingDTO->to = $recipient;
+                $maillingDTO->subject = $emailData['subject'] ?? 'Sujet par défaut';
+                $maillingDTO->template = $emailData['template'] ?? 'default_template.html.twig';
+                $maillingDTO->name = $emailData['name'] ?? 'Nom par défaut';
+                $maillingDTO->body = $emailData['body'] ?? 'Corps du message par défaut';
+                $bulkEmailDTOs[] = $maillingDTO;
+            }
 
-        return $this->json($response);
-    }
+            // Envoie des emails en utilisant la méthode sendBulkEmails du service
+            $maillingService->sendBulkEmails($bulkEmailDTOs);
 
-    /**
-     * Sends an email using a predefined template from the database.
-     *
-     * @param mixed $template_id The ID of the email template.
-     * @param MailTemplateRepository $mailTemplateRepository Repository for mail template entity.
-     *
-     * @return JsonResponse Returns a JSON response with the email template body or error message.
-     */
-    #[Route('/send-template/{template_id}', name: 'app_mail_send_template')]
-    public function sendTemplate(
-        $template_id,
-        MailTemplateRepository $mailTemplateRepository
-    ): JsonResponse {
-        $template = $mailTemplateRepository->findOneBy(['id' => $template_id]);
-
-        if ($template) {
-            return $this->json(['status' => 'success', 'message' => 'Modèle d\'e-mail créé avec succès']);
+            return $this->json([
+                'message' => 'Emails envoyés avec succès!',
+            ]);
+        } else {
+            // Gérer le cas où $emailData est null
+            return $this->json([
+                'message' => 'Données JSON invalides dans la requête.',
+            ], JsonResponse::HTTP_BAD_REQUEST);
         }
-
-        return $this->json(['status' => 'error', 'message' => 'Template not found'], 404);
-    }
-
-    /**
-     * Fetches a single email entity based on a given ID and returns its details.
-     *
-     * @param Request $request HTTP request containing the email ID.
-     * @param MailRepository $mailRepository Repository for mail entity.
-     *
-     * @return JsonResponse Returns a JSON response with email details or an error message.
-     */
-    #[Route('/single', name: 'app_mail_single')]
-    public function singleEmail(Request $request, MailRepository $mailRepository): JsonResponse {
-        $email = $mailRepository->findOneBy(['id' => '018bf611-344a-7d18-af75-12bcfba983f0']);
-
-        if ($email) {
-            return $this->json([$email]);
-        }
-
-        return $this->json(['status' => 'error', 'message' => 'Email not found'], 404);
     }
 }
--- src/Service/MaillingService.php
+++ src/Service/MaillingService.php
@@ -1,32 +1,60 @@
 <?php
+
 namespace App\Service;
 
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 
-class EmailService
+/**
+ * Service for sending emails.
+ */
+class MaillingService
 {
     private $mailer;
 
+    /**
+     * Constructor.
+     *
+     * @param MailerInterface $mailer The Mailer interface to send emails.
+     */
     public function __construct(MailerInterface $mailer)
     {
         $this->mailer = $mailer;
     }
 
-    public function sendEmail(string $from, string $to, string $subject, string $template, string $name, string $body = ''): void
+    /**
+     * Send multiple emails in batches with a pause interval.
+     *
+     * @param array $emailDTOs An array of MaillingDTO objects.
+     * @param int $batchSize The number of emails to send in each batch.
+     * @param int $pauseInterval The pause interval (in seconds) between batches.
+     */
+    public function sendBulkEmails(array $emailDTOs, int $batchSize = 10, int $pauseInterval = 11): void
     {
-        $email = (new TemplatedEmail())
-            ->from($from)
-            ->to(new Address($to))
-            ->subject($subject)
-            ->htmlTemplate('email/' . $template)
-            ->context([
-                'expiration_date' => new \DateTime('+7 days'),
-                'username' => $name,
-                'body' => $body,
-            ]);
+        $emailBatches = array_chunk($emailDTOs, $batchSize);
 
-        $this->mailer->send($email);
+        foreach ($emailBatches as $batch) {
+            foreach ($batch as $emailDTO) {
+                $email = (new TemplatedEmail())
+                    ->from($emailDTO->from)
+                    ->to(new Address($emailDTO->to))
+                    ->subject($emailDTO->subject)
+                    ->htmlTemplate('email/' . $emailDTO->template)
+                    ->context([
+                        'expiration_date' => new \DateTime('+7 days'),
+                        'username' => $emailDTO->name,
+                        'body' => $emailDTO->body,
+                    ]);
+
+                try {
+                    $this->mailer->send($email);
+                } catch (TransportExceptionInterface $e) {
+                }
+            }
+
+            sleep($pauseInterval);
+        }
     }
 }
