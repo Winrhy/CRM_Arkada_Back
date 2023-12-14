0de55911090467bd376b4adad5c01f0b9b485741
Winrhy <Winrhy@gmail.com>
2023-12-14 11:02:46 +0100

    :sparkles: improve segment controller in SegmentController

--- src/Controller/Api/SegmentController.php
+++ src/Controller/Api/SegmentController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Api;
+namespace App\Controller;
 
 use App\Service\SegmentService;
 use App\DTO\SegmentDTO;
@@ -42,5 +42,56 @@ class SegmentController extends AbstractController
         return $this->json($segment, Response::HTTP_CREATED);
     }
 
+    /**
+     * Retrieves a Segment entity by its ID.
+     *
+     * @Route("/{id}", name="get_segment", methods={"GET"})
+     * @param string $id The Segment entity ID.
+     * @return Response The HTTP response.
+     */
+    public function getSegment(string $id): Response
+    {
+        $segment = $this->segmentService->getSegment($id);
+        return $this->json($segment);
+    }
 
+    /**
+     * Updates an existing Segment entity identified by ID.
+     *
+     * @Route("/{id}", name="update_segment", methods={"PUT"})
+     * @param string $id The Segment entity ID.
+     * @param Request $request The HTTP request.
+     * @return Response The HTTP response.
+     */
+    public function updateSegment(string $id, Request $request): Response
+    {
+        $segmentDTO = $this->jsonToObject($request->getContent(), SegmentDTO::class);
+        $segment = $this->segmentService->updateSegment($id, $segmentDTO);
+        return $this->json($segment);
+    }
+
+    /**
+     * Deletes a Segment entity identified by ID.
+     *
+     * @Route("/{id}", name="delete_segment", methods={"DELETE"})
+     * @param string $id The Segment entity ID.
+     * @return Response The HTTP response.
+     */
+    public function deleteSegment(string $id): Response
+    {
+        $this->segmentService->deleteSegment($id);
+        return $this->json(null, Response::HTTP_NO_CONTENT);
+    }
+
+    /**
+     * Helper function to deserialize JSON content to an object.
+     *
+     * @param string $json The JSON string.
+     * @param string $class The class to deserialize into.
+     * @return object The deserialized object.
+     */
+    private function jsonToObject(string $json, string $class)
+    {
+        return $this->get('serializer')->deserialize($json, $class, 'json');
+    }
 }
