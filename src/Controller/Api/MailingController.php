71e1ac0a696c791c36d215143bf939130fae52fd
Paul_Delamare <paulodela2006@gmail.com>
2023-12-14 20:45:53 +0100

    Working Email

--- src/Controller/Api/MailingController.php
+++ src/Controller/Api/MailingController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller\Api;
 
+use App\Repository\UserRepository;
 use App\Service\MaillingService;
 use App\DTO\MaillingDTO;
 use App\Entity\Mail;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
 
 #[Route('/mail', name: 'app_mail')]
 class MailingController extends AbstractController
@@ -21,9 +25,11 @@ class MailingController extends AbstractController
      *
      * @param MaillingService $MaillingService Service for sending emails.
      */
-    public function __construct(MaillingService $MaillingService)
+    public function __construct(MaillingService $MaillingService, TokenStorageInterface $tokenStorageInterface, JWTTokenManagerInterface $jwtManager)
     {
         $this->MaillingService = $MaillingService;
+        $this->jwtManager = $jwtManager;
+        $this->tokenStorageInterface = $tokenStorageInterface;
     }
 
     /**
@@ -37,18 +43,24 @@ class MailingController extends AbstractController
      * @return JsonResponse Returns a JSON response after creating and sending the email.
      */
     #[Route('/create', name: 'app_mail_create')]
-    public function createEmail(Request $request, MaillingService $maillingService, EntityManagerInterface $entityManager): JsonResponse {
+    public function createEmail(Request $request, MaillingService $maillingService, EntityManagerInterface $entityManager,UserRepository $userRepository): JsonResponse {
         $emailData = json_decode($request->getContent(), true);
 
         if ($emailData !== null) {
             foreach ($emailData['to'] as $recipient) {
                 $maillingDTO = new MaillingDTO();
                 $maillingDTO->from = $emailData['senderMail'];
-                $maillingDTO->to = $recipient;
+                $maillingDTO->to = $recipient['email'];
                 $maillingDTO->subject = $emailData['subject'] ?? 'Sujet par défaut';
                 $maillingDTO->template = $emailData['templateName'] ?? 'default_template.html.twig';
-//                $maillingDTO->name = $emailData['name'] ?? 'Nom par défaut';
                 $maillingDTO->body = $emailData['body'] ?? 'Corps du message par défaut';
+                $maillingDTO->name = $recipient['firstname'] ?? '';
+                $maillingDTO->last_name=$recipient['lastname'] ?? '';
+                $maillingDTO->password = $emailData['password'] ??'';
+
+
+                $decodedJwtToken = $this->jwtManager->decode($this->tokenStorageInterface->getToken());
+                $user = $userRepository->findOneBy(['email' => $decodedJwtToken['username']]);
 
                 $mail = new Mail();
                 $mail->setSubject($maillingDTO->subject);
@@ -57,16 +69,18 @@ class MailingController extends AbstractController
                 $mail->setSenderMail($maillingDTO->from);
                 $mail->setReceiver($maillingDTO->to);
                 $mail->setRead(false);
+                $mail->setUserId($user);
 
                 $entityManager->persist($mail);
 
+                $maillingDTO->email_id=$mail->getId();
                 $bulkEmailDTOs[] = $maillingDTO;
             }
 
             $entityManager->flush();
 
-            $maillingService->sendBulkEmails($bulkEmailDTOs);
-
+            $ok = $maillingService->sendBulkEmails($bulkEmailDTOs);
+            return $this->json($ok);
             return $this->json([
                 'message' => 'Emails envoyés avec succès!',
             ]);
